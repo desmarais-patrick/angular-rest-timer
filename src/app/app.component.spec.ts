2448ec9ad126b99541841ec4a6f623a198977a0d
Patrick Desmarais <patrick.desm@gmail.com>
2022-11-10 16:39:53 -0500

    Fix app component tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-rest-timer'`, () => {
+  it(`should have as title 'Rest Timer'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-rest-timer');
+    expect(app.title).toEqual('Rest Timer');
   });
 
-  it('should render title', () => {
+  it('should render footer', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('angular-rest-timer app is running!');
+    expect(compiled.querySelector('rt-footer')).toBeTruthy();
   });
 });
